b7a3298f8fea1dcf59c3887c5415b638db8a6299
4souravsinha <4souravsinhain@gmail.com>
2022-12-13 19:06:11 +0530

    Setup src/config/serverConfig.js and started the server
    We setup the enviroment variables by creating .env in the main repo and serverconfig.js in src/config.
    We made PORT an env variable to be accessed through proccess.env.PORT. This was possible due to dotenv module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const app = express();
+const {PORT} = require('./config/serverConfig.js')
 
 const setupAndStartServer = async => {
-    
+    app.listen(PORT, () => {
+        console.log(`server started at port ${PORT}`);
+    })
 }
+setupAndStartServer();
