5c7c3dced70582e369dbfe147707d5095991d3ca
4souravsinha <4souravsinhain@gmail.com>
2022-12-17 00:21:57 +0530

    completed city repository
    We did CRUD operations using sequelize for city repository.

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -6,26 +6,55 @@ class CityRepository {
     async createCity({ CityName }) {
 
         try {
-            await City.create({
+            const city = await City.create({
                 name: CityName
             });
+            return city;
         } catch (error) {
+            console.log("something went wrong in repository layer");
             throw error;
         }
     }
 
     async deleteCity(CityId) {
-
         try {
             await City.destroy({
                 where: {
                     id: CityId
                 }
             });
+            return true;
         } catch (error) {
-            throw error;
+            console.log("something went wrong in repository layer");
+            throw { error };
         }
 
+    }
+
+    async updateCity(cityId, data) {
+
+        try {
+            const city = await City.update({ data }, {
+                where: {
+                    id: cityId
+                }
+            });
+            return city;
+        } catch (err) {
+            console.log("something went wrong in repository layer");
+            throw { err };
+        }
+    }
+
+    async getCity(cityId) {
+
+        try {
+            const city = await City.findByPk(cityId);
+            return city;
+        } catch (err) {
+            console.log("something went wrong in repository layer");
+            throw { err };
+        }
 
     }
 }
