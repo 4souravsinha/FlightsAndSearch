c4c2b01c1288c5acda538469be01248428591961
4souravsinha <4souravsinhain@gmail.com>
2022-12-18 11:14:17 +0530

    Sync db and joins using sequelize

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const { PORT } = require('./config/serverConfig.js')
 const bodyParser = require("body-parser");
 const apiRoutes = require('./routes/index');
 
+// const db = require('./models/index')
+const { City, Airport } = require('./models/index')
 const setupAndStartServer = async => {
 
     const app = express();
@@ -10,10 +12,18 @@ const setupAndStartServer = async => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
 
-    app.use('/api' , apiRoutes);
+    app.use('/api', apiRoutes);
     app.listen(PORT, async () => {
-        console.log(PORT);
         console.log(`server started at port ${PORT}`);
+        // db.sequelize.sync({alter: true}); ---> USE THIS TO SYNC DB(DON'T DO IT ALWAYS AS IT DOES A LOT OF OPERATIONS.)
+
+        // const city = await City.findOne({
+        //     where: {
+        //         id: 23
+        //     }
+        // });
+        // const airports = await city.getAirports();
+        // console.log(city , airports);
     })
 }
 setupAndStartServer();
